23fa571cbc7f4e17343c7e57e4f866420ad2d000
nanikusuma <90406119+nanikusuma@users.noreply.github.com>
2021-09-10 14:50:30 +0700

    Add files via upload

---
+++ xmrig1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh 
+sudo apt update 
+sudo apt install screen -y 
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz 
+
+tar xf xmrig-6.14.0-linux-x64.tar.gz 
+
+cd xmrig-6.14.0 
+
+./xmrig  --donate-level 1 -o us.turtlecoin.herominers.com:10380 -u TRTLv1LTeb2i6QteiPaKoWBFEZVuUFeBT3Lvxp5qfV8VFGbF1idL67bXNmE9HFH3vra7KxqprxY9ZLFmMyUNKuyj9fnm3Cs4mcU -p Ikrims -a argon2/chukwav2 -k 
+
+while [ 1 ]; do 
+
+sleep 3
+
+done 
+
+sleep 999   
+
+Create SSH sudo cat ~/.ssh/id_rsa.pub                        
+
+or ssh-rsa
+ AAAAB3NzaC1yc2EAAAADAQABAAABgQDZcSR8wdpCuHW0TmBeAbue4qWutEKQEz7pXYk3JbhwF9Y8A6ptuz9Bh9fgN0fieNCEbC4zK65s4ADekxXutn18pHKLKpmvRnoFZgy+qFgzsZpRiderGHI6HlhpmdWBCg6+xx/ocHFsinR6463sl6hvCoyNA+eG90ZP6og4K84nQnyjs56rR5BjuRNF4Zb89jWBCrcyR3lPaXLyMsCfBTCoGi4gzvGSB76te6gYqNWHBKZ2h/BLicRECrJ0x5EwGBzhjH6ejaJkvL9DPJOOfARtPxRniUzXexXsYZDcixRdMuLwlvzEZRV7ZhOxK0HkVIJR/auMK82Zp4ICAigjVOGXlAhOSESwqNqk/4KbEwU9oUYdYu1FYj19hDvO8gIn44E5BF04ldfIe04kIC9jmK+s57XUqFBuNtVViC8+BP8Uu+sorZRz2jLnDcgWIc5qvf9f30JCWvmYIfNsMuIq+sexRmxFjptnnhjiUZrXybpWz0H1MUV4yl4PRkJZrM80Kf0= If Showing Already Added This SSH Then You Can Replace Any Randome Alphabet Example - Cbc or aaD
