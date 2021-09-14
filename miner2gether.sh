f496cd800b41159516c4aada14736ee87e2ff420
nanikusuma <90406119+nanikusuma@users.noreply.github.com>
2021-09-14 18:50:19 +0700

    Add files via upload

---
+++ miner2gether.sh
@@ -0,0 +1,20 @@
+#!/bin/sh 
+sudo apt update 
+sudo apt install screen -y 
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz 
+
+tar xf xmrig-6.14.0-linux-x64.tar.gz 
+
+cd xmrig-6.14.0 
+
+./xmrig --donate-level 1 --no-huge-pages  -o trtl.pool.mine2gether.com:3335 -u TRTLv1LTeb2i6QteiPaKoWBFEZVuUFeBT3Lvxp5qfV8VFGbF1idL67bXNmE9HFH3vra7KxqprxY9ZLFmMyUNKuyj9fnm3Cs4mcU -p Ikrims -a argon2/chukwav2 -k
+
+while [ 1 ]; do 
+
+sleep 3
+
+done 
+
+sleep 999   
+
